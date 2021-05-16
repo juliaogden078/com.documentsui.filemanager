7f6e9ea9f6cac74b9da0b33afde631374163981e
Laptopuser <es1@example.com>
2021-05-16 10:35:55 +0200

    inital

---
+++ README.md
@@ -0,0 +1,18 @@
+# Storage-USB
+
+
+Simple app (15KB) to launch Storage Settings directly from the apps drawer
+
+![ScreenShot](https://raw.githubusercontent.com/documentsuicid/Storage-USB/master/art/icon2.png)
+ 
+F-Droid
+ 
+<a href="https://f-droid.org/repository/browse/?fdid=com.documentsui.filemanager">
+  <img alt="Get it on F-Droid"       src="https://raw.githubusercontent.com/documentsuicid/Storage-USB/master/art/fdroid.png" />
+</a>
+
+
+
+![ScreenShot](https://raw.githubusercontent.com/documentsuicid/Storage-USB/master/art/art2.png)
+
+You can download the latest (2.0.3) signed APK from this repo here: https://github.com/documentsuicid/Storage-USB/raw/master/project/app/app-release.apk
---
+++ app/src/main/java/com/documentsui/filemanager/MainActivity.java
@@ -0,0 +1,32 @@
+package com.documentsui.filemanager;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+
+    Intent intent = new Intent();
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+//        intent.setAction(android.settings.INTERNAL_STORAGE_SETTINGS);
+
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.setType("*/*");
+        intent.setClassName("com.android.documentsui", "com.android.documentsui.ViewDownloadsActivity");
+//        intent.setClassName("com.android.documentsui", "com.android.documentsui.files.FilesActivity");
+//        intent.setClassName("com.android.documentsui", "com.android.documentsui.ScopedAccessActivity");
+        startActivity(intent);
+
+        if (Build.VERSION.SDK_INT >= 21) {
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+        }
+
+        finish();
+        super.onCreate(savedInstanceState);
+    }
+
+}
